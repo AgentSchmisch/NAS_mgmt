82aebdade5c32f9d11b688b86ba26efd5c46b24c
Florian Schmid <florian.schmid2002@outlook.com>
2023-05-24 13:53:56 +0200

    :sparkles: logs now sent to server

--- logger.py
+++ logger.py
@@ -1,7 +1,8 @@
 import logging
-import time
+from datetime import datetime
 import config_helper
 import requests
+
 """ Log levels
 Debug
 info
@@ -10,57 +11,56 @@ error
 critical
 """
 
+class HTTPLogHandler(logging.Handler):
+    def __init__(self, url):
+        super().__init__()
+        self.url = url
+
+    def emit(self, record):
+        log_entry = self.format(record)
+        payload = {'log_entry': log_entry}
+
+        try:
+            response = requests.post(self.url, json=payload)
+            # Check the response status code or handle the response as needed
+        except requests.exceptions.RequestException as e:
+            # Handle any exceptions that occurred during the HTTP request
+            print("Failed to send log entry:", str(e))
+
 def logger(level,name):
+
     config_obj = config_helper.load_conf()
     log_folder = config_obj["folders"]["log_path"]
+    log_url = config_obj["addresses"]["logs"]
 
-    filename = "log_", datetime.today().strftime('%Y_%m_%d')
+    filename = log_folder + "log_"+ str(datetime.now().date()).replace("-", "_")
     logger = logging.getLogger(name)
     logger.setLevel(level)
-    # create console handler and set level to debug
-    ch = logging.StreamHandler()
-    ch.setLevel(logging.DEBUG)
 
-    # create formatter
+    http_logger = HTTPLogHandler(log_url)
+    http_logger.setLevel(level)
+
     formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+    http_logger.setFormatter(formatter)
 
-    # add formatter to ch
-    ch.setFormatter(formatter)
-    
-    # add ch to logger
-    logger.addHandler(ch)
     logging.basicConfig(filename=filename, encoding='utf-8', format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
 
     return logger
 
-def log(level,name,text):
-
-    #TODO create log function to log into the file as well as send the level to nodered
-
-
-    log = logger(logging.DEBUG,name)
-
-    if level == "info":
-        log.info(text)
-        send_logs_to_nodered(level,name)
-        return 1
-    elif level == "warning":
-        log.warning(text)
-        send_logs_to_nodered(level,name)
-        return 2
-    elif level == "error":
-        log.error(text)
-        send_logs_to_nodered(level,name)
-        return 3
-    elif level == "critical":
-        log.critical(text)
-        send_logs_to_nodered(level,name)
-        return 4
-    else:
-        return "not logged"
 
 def cleanup_old_logs():
+    # this function will be executed every day at 12pm with a Background 
     #TODO: check if there are logs older than x days, and delete them
+    config_obj = config_helper.load_conf()
+    log_folder = config_obj["folders"]["log_path"]
+
+    log_files = os.listdir(log_folder)
+    for log_file in log_files:
+        log_date_raw = log_file.split("_")
+        log_date = date(log_date_raw[1], log_date_raw[2], log_date_raw[3])
+        if datetime.now().date - timedelta(days=7) > log_date:
+            os.remove(log_folder + log_file)
+
     log = logger(logging.DEBUG,"logs")
     log.info("deleted old logs")
     return true
@@ -69,5 +69,5 @@ def send_logs_to_nodered(level, name):
     config_obj = config_helper.load_conf()
     nodered = config_obj["addresses"]["nodered"]
     requests.post("http://"+ nodered +"/api/v1/logging", log)
-    #TODO Create request that will push formatted data from logs to nodered where it will be presented more appealing
+    #TODO Create post requests that will push formatted data from logs to nodered where it will be presented more appealing
     return true
