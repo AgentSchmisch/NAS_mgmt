403a2420702ca9491c934006a43065b44bfdd8b2
Florian Schmid <florian.schmid2002@outlook.com>
2023-05-24 13:52:45 +0200

    :spakkles: ftp now using multiple users

--- ftp.py
+++ ftp.py
@@ -1,11 +1,16 @@
 import os
-from pyftplib import DummyAuthorizer, FTPHandler, FTPServer
-import functions
-import config_helper
-import logging
-from logger import logger
+import threading
+from pyftpdlib.authorizers import DummyAuthorizer
+from pyftpdlib.handlers import FTPHandler
+from pyftpdlib.servers import ThreadedFTPServer
 
-log = logger(logging.DEBUG, "ftp")
+import functions
+from config_helper import load_conf
+import logging
+from logger import logger,http_logger
+
+log = logger(logging.INFO, "ftp")
+
 
 class ftphandler(FTPHandler):
     def on_file_recieved(self,file):
@@ -16,27 +21,33 @@ class ftphandler(FTPHandler):
         if self.username == "camera":
             if file_extension in functions.allowed_image_types:
                 # TODO could try to execute the sort_new_images function, possibility is that there will be images that drop under the table bc of the images being transferred faster than they are converted
+                functions.sort_new_images(file)
                 print("allowed file")
-                log.info("%u uploaded file %f",self.username,file)
+                log.info("%s uploaded file %s"%(self.username,file))
             else:
                 os.remove(file)
-                self.response("550 File %f is not allowed to be uploaded as user: %u", file, self.username)
-                log.warning(msg)("user %u tried to upload an illegal file of type %f", self.username, file_extension)
+                self.response("550 File %s is not allowed to be uploaded as user: %s"%( file, self.username))
+                log.warning("user %s tried to upload an illegal file of type %s"% (self.username, file_extension))
 
     def on_incomplete_file(self,file):
         print(file," incomplete")
-        log.warning("file %f was not uploaded completely", file) 
+        log.warning("file %s was not uploaded completely"%file) 
 
-config_obj = load_conf()
-ftp_path = config_obj["folders"]["ftp_path"]
+def init_ftp_server():
+    print("init")
+    #log.INFO("started ftp server")
+    config_obj = load_conf()
+    ftp_path = config_obj["folders"]["ftp_path"]
 
-authorizer = DummyAuthorizer()
-
-for user in config_obj["ftp_users"]: # creating all the users in the config file
-    authorizer.add_user(user["ftp_username"], user["ftp_password"], fpt_path, perm="elradfmwMT")
-
-handler = MyFTPHandler
-handler.authorizer = authorizer
-
-# Create the FTP server
-server = FTPServer(("0.0.0.0", 21), handler)
+    authorizer = DummyAuthorizer()
+    for user in config_obj["ftp_users"]:
+     authorizer.add_user(user["ftp_username"], user["ftp_username"], ftp_path, perm="elradfmwMT")
+    handler = ftphandler
+    handler.authorizer = authorizer
+    try:
+        # Create the FTP server
+        server = ThreadedFTPServer(("0.0.0.0", 21), handler)
+        server.serve_forever()
+    except Exception as ex:
+        log.error("Error: "+ex)
+        
